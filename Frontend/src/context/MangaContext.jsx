77d6404d07b2f8dcfc325a4acdabc5eeaa936744
Abdessamadds <Ahmedbosofa1997@gmail.com>
2024-04-02 10:44:46 +0000

    Context & useReducer

--- Frontend/src/context/MangaContext.jsx
+++ Frontend/src/context/MangaContext.jsx
@@ -2,11 +2,33 @@ import { createContext, useReducer } from "react";
 
 export const MangaContext = createContext();
 
-export const mangasReducer = (state, action) => {};
+export const mangasReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_MANGAS":
+      return {
+        mangas: action.payload,
+      };
+    case "CREATE_MANGAS":
+      return {
+        mangas: [action.payload, ...state.mangas]
+      }
+      default:
+        return state
+  }
+};
 
 export const MangaContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(mangasReducer, {
     mangas: null,
   });
-  return <MangaContext.Provider>{children}</MangaContext.Provider>;
-};
+  /* dispatch: it's a function that updates the state, within it, we pass an object as argument, 
+  first propretie called type  ( REQUIRED )  ( string with all Uppercase letters and underScore ex: ADD_COUNT) which describe the state change we want to make.
+  Second propertie called payload ( Optional ) which represents  Any additional data that the reducer function needs to perform the update
+  */
+  return 
+  (
+  <MangaContext.Provider>
+   {children}
+   </MangaContext.Provider>
+  )
+}
